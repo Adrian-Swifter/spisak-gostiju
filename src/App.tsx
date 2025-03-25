23627791d6128fd6f71f6e32424c41c9ae69db16
adrian-swifter <milosffh@yahoo.com>
2025-03-25 01:14:58 +0100

    added titles to setting tab button and added contact

--- src/App.tsx
+++ src/App.tsx
@@ -256,9 +256,9 @@ const App = () => {
             </div>
             <div
               style={{
-                marginBottom: "80px",
                 overflowY: "auto",
-                height: "calc(100vh - 200px)",
+                height: "calc(100vh - 180px)",
+                padding: "20px",
               }}
             >
               {activeTab === "guests" && (
@@ -275,97 +275,118 @@ const App = () => {
               )}
               {activeTab === "settings" && (
                 <div
-                  className="button-group"
                   style={{
                     display: "flex",
                     flexDirection: "column",
-                    gap: "10px",
-                    marginTop: "20px",
+                    justifyContent: "space-between",
+                    height: "100%",
                   }}
                 >
-                  <label
+                  <div
+                    className="button-group"
                     style={{
-                      ...buttonStyle,
-                      backgroundColor: "#fff",
-                      color: "#000",
                       display: "flex",
-                      alignItems: "center",
-                      gap: "5px",
-                      cursor: "pointer",
-                      border: "1px solid #ccc",
+                      flexDirection: "column",
+                      gap: "10px",
+                      marginTop: "20px",
                     }}
                   >
-                    <FaFileImport style={iconStyle} />
-                    Uvezite Konfiguraciju
-                    <input
-                      type="file"
-                      onChange={importData}
-                      accept=".json"
+                    <label
                       style={{
-                        display: "none",
+                        ...buttonStyle,
+                        backgroundColor: "#fff",
+                        color: "#000",
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "5px",
+                        cursor: "pointer",
+                        border: "1px solid #ccc",
                       }}
-                    />
-                  </label>
-                  <button
-                    onClick={exportData}
-                    style={{
-                      ...buttonStyle,
-                      backgroundColor: "#fff",
-                      color: "#000",
-                      border: "1px solid #ccc",
-                    }}
-                  >
-                    <FaFileExport style={iconStyle} />
-                    Sačuvaj Konfiguraciju
-                  </button>
-                  <button
-                    onClick={exportExcel}
-                    style={{
-                      ...buttonStyle,
-                      backgroundColor: "#fff",
-                      color: "#000",
-                      border: "1px solid #ccc",
-                    }}
-                  >
-                    <FaFileExcel style={iconStyle} />
-                    Izvezi Excel Spisak Gostiju
-                  </button>
-                  <button
-                    onClick={exportPDF}
-                    style={{
-                      ...buttonStyle,
-                      backgroundColor: "#fff",
-                      color: "#000",
-                      border: "1px solid #ccc",
-                    }}
-                  >
-                    <FaFilePdf style={iconStyle} />
-                    Izvezi PDF Spisak Gostiju
-                  </button>
-                  <button
-                    onClick={exportCanvasToPDF}
-                    style={{
-                      ...buttonStyle,
-                      backgroundColor: "#fff",
-                      color: "#000",
-                      border: "1px solid #ccc",
-                    }}
-                  >
-                    <FaChair style={iconStyle} />
-                    Izvezi Plan Sale PDF
-                  </button>
-                  <button
-                    onClick={() => setShowResetPopup(true)}
-                    style={{
-                      ...buttonStyle,
-                      backgroundColor: "#fff",
-                      color: "#000",
-                      border: "1px solid #ccc",
-                    }}
-                  >
-                    <FaRedo style={iconStyle} />
-                    Resetuj Sve
-                  </button>
+                      title="Fajl koji ste sačuvali. Završava se sa .json ekstenzijom."
+                    >
+                      <FaFileImport style={iconStyle} />
+                      Uvezite Konfiguraciju
+                      <input
+                        type="file"
+                        onChange={importData}
+                        accept=".json"
+                        style={{
+                          display: "none",
+                        }}
+                      />
+                    </label>
+                    <button
+                      onClick={exportData}
+                      style={{
+                        ...buttonStyle,
+                        backgroundColor: "#fff",
+                        color: "#000",
+                        border: "1px solid #ccc",
+                      }}
+                      title="Sačuvajte podatke (goste,stolove...) za kasnije korišćenje."
+                    >
+                      <FaFileExport style={iconStyle} />
+                      Sačuvaj Konfiguraciju
+                    </button>
+                    <button
+                      onClick={exportExcel}
+                      style={{
+                        ...buttonStyle,
+                        backgroundColor: "#fff",
+                        color: "#000",
+                        border: "1px solid #ccc",
+                      }}
+                    >
+                      <FaFileExcel style={iconStyle} />
+                      Izvezi Excel Spisak Gostiju
+                    </button>
+                    <button
+                      onClick={exportPDF}
+                      style={{
+                        ...buttonStyle,
+                        backgroundColor: "#fff",
+                        color: "#000",
+                        border: "1px solid #ccc",
+                      }}
+                    >
+                      <FaFilePdf style={iconStyle} />
+                      Izvezi PDF Spisak Gostiju
+                    </button>
+                    <button
+                      onClick={exportCanvasToPDF}
+                      style={{
+                        ...buttonStyle,
+                        backgroundColor: "#fff",
+                        color: "#000",
+                        border: "1px solid #ccc",
+                      }}
+                    >
+                      <FaChair style={iconStyle} />
+                      Izvezi Plan Sale PDF
+                    </button>
+                    <button
+                      onClick={() => setShowResetPopup(true)}
+                      style={{
+                        ...buttonStyle,
+                        backgroundColor: "#fff",
+                        color: "#000",
+                        border: "1px solid #ccc",
+                      }}
+                      title="Briše sve podatke (goste, stolove...). Koristiti u slučaju potrebe za rasporedom gostiju u dodatnoj sali i slično."
+                    >
+                      <FaRedo style={iconStyle} />
+                      Resetuj Sve
+                    </button>
+                  </div>
+                  <div>
+                    <p>
+                      Za dodatne funkcionalnosti ili za prijavu problema pri
+                      korišćenju, molimo pošaljite email na{" "}
+                      <a href="mailto:contact@milosdraskovic.com">
+                        contact@milosdraskovic.com
+                      </a>
+                    </p>
+                  </div>
                 </div>
               )}
 
@@ -678,6 +699,7 @@ const App = () => {
                   color: "#000",
                   border: "1px solid #ccc",
                 }}
+                title="Sačuvajte podatke (goste,stolove...) za kasnije korišćenje."
               >
                 <FaFileExport style={iconStyle} />
                 Sačuvaj konfiguraciju
